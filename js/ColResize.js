2567a5b5fa542ac290076def17543193e7a6fe22
etoah <etoahn@163.com>
2015-08-31 00:02:25 +0800

    实现功能，待优化

---
+++ js/ColResize.js
@@ -0,0 +1,111 @@
+/**
+ * Created by Lucien on 8/30/2015.
+ */
+"use strict";
+(function(window,document){
+
+
+    var IGNORE_TABLE_ATRR="data-col-resize";
+    var RESIZE_OFFSET=12;
+
+    function addEvent(element,event,callback)
+    {
+        if(element.addEventListener) //DOM 2
+        {
+            element.addEventListener(event,callback);
+
+        }
+        else if(element.attachEvent)//less than IE9
+        {
+            element.addEventListener('on'+event,callback);
+        }
+        else//dom 0
+        {
+            element['on'+event]=callback;
+        }
+    }
+
+    function getTableArray()
+    {
+        return document.getElementsByTagName("table");
+    }
+
+    function mousedownListener(event)
+    {
+        if (Math.abs(event.offsetX - this.offsetWidth)<RESIZE_OFFSET)
+        {
+            this.IsMouseDown = true;
+            this.clickX=event.x;
+            this.oldWidth = this.offsetWidth;
+        }
+
+    }
+
+    function mouseupListener(event)
+    {
+        this.IsMouseDown = false;
+        this.style.cursor = 'default';
+    }
+
+    function mousemoveListener(event)
+    {
+       // alert("adfasdf");
+        var j,table;
+        //更改鼠标样式
+        if (Math.abs(event.offsetX - this.offsetWidth)<RESIZE_OFFSET)
+            this.style.cursor = 'col-resize';
+        else
+            this.style.cursor = 'default';
+
+        if(this.IsMouseDown&&(this.oldWidth + (event.x - this.clickX))> 0)
+        {
+
+           this.width = this.oldWidth + (event.x - this.clickX);
+           //调整列宽
+           this.style.width = this.width;
+           this.style.cursor = 'col-resize';
+            //调整该列中的每个Cell
+            table = this.parentElement.parentElement;
+            while (table.tagName != 'TABLE')//兼容tbody
+            {
+                table = table.parentElement;
+            }
+            for (j = 0; j < table.rows.length; j++) {
+                table.rows[j].cells[this.cellIndex].width = this.width;
+            }
+
+        }
+    }
+
+    function addColResizeEvent()
+    {
+        var tables=getTableArray(),
+            i= 0,
+            j= 0,
+            currentCell=null;
+        for(;i<tables.length;i++)
+        {
+            if(tables[i].getAttribute(IGNORE_TABLE_ATRR)==false) {
+            continue;
+            }
+            for(j=0;j<tables[i].rows[0].cells.length;j++)
+            {
+                currentCell=tables[i].rows[0].cells[j];
+                addEvent(currentCell,"mousemove",mousemoveListener);
+                addEvent(currentCell,"mousedown",mousedownListener);
+                addEvent(currentCell,"mouseup",mouseupListener);
+
+
+            }
+
+
+        }
+    }
+
+
+
+    addEvent(window,"load",addColResizeEvent);
+
+
+
+})(window,document);
